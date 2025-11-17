5aed73204bbfd22ae9b628218d75e1e8fdd52b3f
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-18 03:01:13 +0500

    variables

---
+++ 01-variables/script.js
@@ -0,0 +1,46 @@
+// console is used to print a message 
+console.log("Sam");
+
+// comment => part of code which are not going to execute.
+// this is single line comment 
+
+/*
+this is multi line comment
+this is sam
+*/
+
+// VARIABLE => A variable is a like a container that holds data. it is used to store data 
+// that can be used and manipulated throughout your program.
+
+// let name = "sam";
+// console.log(name);
+
+let age = 23;
+console.log(age);
+// THREE STAGES OF VARIABLE 
+// 1. DECLARATION=>
+// 2. INITIALIZATION=>
+// 3. USE => 
+
+// LET, VAR, CONST 
+// let a = 1;
+// let a = 2;
+// console.log(a);
+
+// var a = 2;
+// var b = 3;
+// console.log(b);
+
+// PRIMITIVE DATA TYPES /* NNSSBU */
+
+// q1.
+// let price1 = 200;
+// let price2 = 400;
+// let totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+// q2.
+let firstName = prompt("Enter first name");
+let secondName = prompt("Enter last name");
+let fullName = firstName + " " + secondName;
+console.log(fullName);
