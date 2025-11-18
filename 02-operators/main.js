b6d5990c11ad532f4bdde4e77883cd26fd459463
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-19 00:03:08 +0500

    operators

---
+++ 02-operators/main.js
@@ -0,0 +1,74 @@
+// OPERATOR => an operator is something that tells the computer to do something with the values.
+// let result = 3 + 2;
+// console.log(result);
+
+// ARITHMETIC OPERATOR 
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 / 3);
+// console.log(2 ** 3);
+// console.log(2 * 3);
+// console.log(2 % 3);
+// console.log(3 % 2);
+
+
+// qs.1 
+// let price = 150;
+// let quantity = 3;
+
+// let totalPrice = price * quantity;
+
+// let discount = (totalPrice * 10) / 100;
+
+// let discountedPrice = totalPrice - discount;
+// console.log(discountedPrice);
+
+
+// ASSIGNMENT OPERATOR :
+// let val = 23;   /* assignment operator he ye */
+// val+=2;
+// val-=1;
+// val*=4;
+// val/=10;
+// console.log(val);
+
+
+// COMPARISON OPERATOR 
+// console.log(3 < 2);
+// console.log(10 > 2);
+// console.log(2 <= 2);
+// console.log(8 >= 8);
+
+// console.log(5 == 5);
+// console.log("5" == "5");
+// console.log("5" === 5);
+// console.log("5" != 3);
+
+
+
+// LOGICAL OPERATORS 
+// AND (&&)
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+// OR (||)
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// NOT (!)
+// console.log(!true);
+// console.log(!false);
+
+
+// INCREMENT AND DECREMENT OPERATOR 
+// PRE INCREMENT => ++a
+// POST DECREMENT => a++
+// let a = 5;
+// console.log(a);
+// console.log(a++);
+// console.log(a);
+// console.log(--a);
+
