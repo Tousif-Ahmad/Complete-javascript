53e6dbd8e5b84807bbac45f64b596c7f8097fb6e
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-19 20:10:40 +0500

    practice questions

---
+++ 02-operators/questions.js
@@ -0,0 +1,53 @@
+// q1....
+// let num1 = +prompt("Enter first number..");
+// let num2 = +prompt("Enter second number...");
+// console.log("sum", num1 + num2);
+// console.log("subtraction", num1 - num2);
+// console.log("multiplication", num1 * num2);
+// console.log("division", num1 / num2);
+// console.log("modulo", num1 % num2);
+
+// q2. 
+// let a=b=c = 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// q3. 
+// let num = +prompt("Enter a number");
+// let result = (num % 2 === 0) ? "Even" : "Odd";
+// console.log(result);
+
+// q4. 
+// let num = 10;
+// let ans = (num >= 10 && num <=20) ? "in range" : "not in range";
+// console.log(ans);
+
+// q5. 
+// let dbEmail = "sam@gmail.com";
+// let dbPassword = "sam321";
+// let email = prompt("Enter your email");
+// let password = prompt("Enter your password");
+
+// let result = (email && password === "" || email !== dbEmail && password !== dbPassword) 
+// ? "plz fill all" : "sucessful login"
+// console.log(result);
+
+// q6. 
+// let x = 5;
+// let y = 2;
+// let z = 10;
+
+// x+= y * 3;   /* x = 11 */
+// z /= x;       /* z =  0.9999*/
+// x = z * y;     /* x =  */
+// console.log(x);
+
+
+// q7. 
+// let a = 15;
+// let b = 4;
+// let result = a % b + (a - 3) / 2;
+// console.log(result);    /* 9 */
+
