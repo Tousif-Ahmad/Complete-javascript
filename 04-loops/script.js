0afd4d1a1c9ea4f9bb5c125961d91273f8dbed53
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-20 20:11:40 +0500

    loops

---
+++ 04-loops/script.js
@@ -0,0 +1,50 @@
+// LOOPS => to repeat a block of code again and again.
+ 
+// types of loops :
+// 1. FOR LOOP (syntax)
+// for (initialization, condition, increment/decrment){
+//     body
+// }
+
+// e.g
+// for (let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// 2. WHILE LOOP (syntax):
+
+// while(condition){
+//     // body 
+// }
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+// 3. DO WHILE LOOP (syntax):
+// do{
+// //  body
+// }
+// while(condition)
+
+// let i = 100;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+
+// FOR OF LOOP  (STRING, ARRRAYS , IT GIVES VALUES )
+
+let nam = "sam";
+// for (const ch of nam) {
+//     console.log(ch);
+// }
+
+
+// FOR IN LOOP (OBJECTS, IT GIVES KEYS)
+// for (ch in nam){
+//     console.log(ch);
+// }
