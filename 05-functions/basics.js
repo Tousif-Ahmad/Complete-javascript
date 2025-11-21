7355185a51e185e4bc5635b4ae36c66fe764d7d5
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-21 23:10:31 +0500

    basics

---
+++ 05-functions/basics.js
@@ -0,0 +1,82 @@
+// FUNCTIONS
+// let sum = 0;
+// function myFunc(n) {
+//   for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+//   }
+//   console.log("sum", sum);
+// }
+// myFunc(5)
+// myFunc(10)
+// myFunc(15)
+
+// 
+// function alpha(){
+//     console.log("Hello Bhai");
+//     console.log("kese ho");
+// }
+// alpha();
+// alpha();
+// alpha();
+
+// let age = 23;
+// console.log(age);
+
+// alpha();
+
+// ************* with return ************************************
+// function beta(){
+//     return "SAM"
+// }
+// let result = beta();
+// console.log(result);
+
+// parameter without return
+// function gamma(name){
+//     console.log("Hello", name);
+// }
+// gamma("Sam")
+
+
+// default parameters 
+// function greet(name = "uffexx"){
+//     console.log(name);
+// }
+// greet();
+
+// TYPES OF FUNCTIONS :
+// 1. FUNCTION DECLARATION (NORMAL FUNCTION);
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+// let result = sum(10,20);
+// console.log(result);
+
+// FUNCTION EXPRESSION :
+// let sum = function(num1, num2){
+//     return num1 + num2;
+// }
+// let result = sum(10,20);
+// console.log(result);
+
+// ARROW FUNCTION :
+// let sum = (num1, num2)=>{
+//     return num1 + num2
+// }
+// let result = sum(1,2);
+// console.log(result);
+
+// ANONYMOUS FUNCTION 
+// setTimeout(function(){
+//     console.log("Hello");
+// }, 1000)
+
+// let alpha = function(){
+//     console.log("hello sam");
+// }
+// setTimeout(alpha, 1000)
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE):
+// (function greet(){
+//     console.log("Hello");
+// })()
