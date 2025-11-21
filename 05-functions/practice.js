e99f7ec7d160edd6c01d07c8f6067195e6774e17
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-22 00:37:58 +0500

    practice questions

---
+++ 05-functions/practice.js
@@ -0,0 +1,32 @@
+// q1.        
+// function capitalizeFirstLetter(str){
+//     console.log(str.charAt(0).toUpperCase()+str.slice(1));
+// }
+// capitalizeFirstLetter("sam");
+// capitalizeFirstLetter("uffexx")
+
+// q2. 
+// let login = ()=>{
+//     console.log("Please login");
+// }
+// setTimeout(login, 1000);
+
+// q3. 
+// let calculateDiscountedPrice = function(price, discount){
+//     let discountedPrice = price - price * (discount/100);
+//     return discountedPrice;
+// }
+// let result = calculateDiscountedPrice(100,20);
+// console.log(result);
+
+// q3.  
+// function createUserName(name){
+//     console.log(`@${name}${name.length}`);
+// }
+// createUserName("sam")
+// createUserName("uffexx")
+
+
+
+
+
