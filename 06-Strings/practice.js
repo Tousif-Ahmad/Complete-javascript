a942af1bd44917a4fec0364d545a1246dee030db
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-23 01:15:00 +0500

    string practice

---
+++ 06-Strings/practice.js
@@ -0,0 +1,35 @@
+// q1
+
+// function generateUserName(fullName){
+//     return `@${fullName.trim().replaceAll(" ", "")}_${fullName.length}` 
+// }
+// let result = generateUserName("uffex ahmad");
+// console.log(result);
+
+// q2. 
+// let count = 0;
+// function countPro(str, ch){
+    // for(l of str){
+    //     if (l.toLowerCase() === ch.toLowerCase()){
+    //         count++;
+    //     }
+       
+    // }
+    //  console.log(count);
+
+//     for (let index in str){
+//         if (str[index].toLowerCase() === ch.toLowerCase()){
+//             console.log("index =", index);
+//             count++;
+//         }
+//     }
+//     console.log("Count =", count);
+// }
+// countPro("Alphaa", "a")
+
+
+// q3. count the words present in a given strings..
+// let sentence = "Hello How are you doing";
+// let countWords = sentence.split(" ");
+// console.log(countWords.length);
+
