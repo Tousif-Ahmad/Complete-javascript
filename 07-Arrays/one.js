659d9a13e329c2b1b0473be778c72f3539d6c50f
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-24 13:44:47 +0500

    array basics

---
+++ 07-Arrays/one.js
@@ -0,0 +1,54 @@
+// ARRAY => An array is a list that can store multiple values 
+// in a single variable.
+
+// CREATE ARRAY USING ARRAY LITERAL 
+// let colors = ["red", "blue", "green", "yellow", "pink"];
+// let marks = [20,34,56,98,45,53];
+// let personDetails = ["Sam", 23, "chitral"];
+// console.log(personDetails);
+
+// CREATE ARRAY USING ARRAY CONSTRUCTOR 
+// let arr = new Array("ironman", "spiderman", "thor");
+// console.log(arr);
+
+// array constructor is used when we want 
+// to create an empty array with given length
+
+// let num = new Array(10);
+// console.log(num);
+
+
+//---------------------------- ARRAY PROPERTIES AND METHODS -----------------------------
+let arr = [2,5,"sam", 10];
+// arr.push("999");
+// arr.pop();
+// arr.shift();
+// arr.unshift("100")
+
+// let newArr = arr.slice(1,3);
+// console.log(newArr);
+
+// arr.splice(2,1,"uffexx")
+
+// let arr2 = ["uffex", "aina"];
+// let newArr = arr.concat(arr2);
+// console.log(newArr);
+// let newArr = [...arr2]
+
+// let test = arr.join("_")
+// console.log(test);
+
+// let bool = arr.includes(2);
+// console.log(bool);
+
+// let index = arr.indexOf(2);
+// console.log(index);
+
+// arr.reverse();
+
+// let fruits = ["apple", "mango", "litchie", "guava", "litchie"];
+// let result = fruits.find((item)=>{
+//     return item === "litchie";
+// })
+// console.log(result);
+// console.log(arr);
