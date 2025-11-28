7aae8bd446cd1666953497cd52e101b064a7d106
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-28 19:15:32 +0500

    html selection

---
+++ 10-DOM-Advance/01-Selection-of-html/index.js
@@ -0,0 +1,20 @@
+// TAG NAME
+// let a = document.getElementsByTagName("h1");
+// console.log(a);
+
+// ID NAME 
+// let heading = document.getElementById("heading");
+// console.log(heading);
+
+// CLASS NAME
+// let heading = document.getElementsByClassName("heading");
+// console.log(heading);
+
+// QUERY SELECTOR
+// let a = document.querySelector(".heading");
+// console.log(a);
+
+// QUERY SELECTOR ALL
+// let a = document.querySelectorAll(".heading");
+// console.log(a);
+
