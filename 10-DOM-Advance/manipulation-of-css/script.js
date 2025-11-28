d13d68f75bc471ee399d1291411fca121d04bda4
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-29 00:40:11 +0500

    css manipulation

---
+++ 10-DOM-Advance/manipulation-of-css/script.js
@@ -0,0 +1,27 @@
+let div = document.querySelector("div");
+
+// 1. attribute
+// div.setAttribute("class", "redBg")
+// div.setAttribute("style", "font-size : 2rem")
+
+// 2. css style 
+// div.style.color = "teal";
+
+// 3. css text
+// div.style.cssText = 'background-color : red; font-size : 5rem';
+
+// 4. css classname
+// console.log(div.className);
+// div.className = "redBg";
+// console.log(div.className);
+
+// 5. css classlist
+// div.classList.add("redBg");
+// div.classList.add("blueBg");
+// div.classList.remove("blueBg");
+// let a = div.classList.contains("redBg");
+// console.log(a);
+// div.classList.toggle("blue");
+// div.classList.toggle("blue");
+// console.log(div.className);
+
