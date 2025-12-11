dae55062dc445c292ff378fde84aa9cfaac6de8e
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-12-11 23:42:27 +0500

    date and time

---
+++ 14-Date-and-time/02-date-object/script.js
@@ -0,0 +1,31 @@
+// date and object 
+// let date = new Date();
+// // console.log(date.toString());
+// console.log(Date.now());
+
+// let now = new Date();
+// console.log(now);
+
+// getting parts of date 
+// let date = new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getTimezoneOffset()/60);
+// console.log(date.getMilliseconds());
+
+// we can set as well 
+
+
+
+// timestamps 
+// let date = new Date();
+// console.log(date.getTime()/1000/60/60/24/365);
+
+// let timestamps = Date.now();
+// console.log(timestamps);
+
+// ----------------------------- CALCULATIONS ON DATE AND TIME ------------------------------
