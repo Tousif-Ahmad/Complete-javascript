75e51d5c912159f7d06aba77c2e91bea8c375376
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-12-11 07:26:21 +0500

    number and math object

---
+++ 14-Date-and-time/01-Number-object/script.js
@@ -0,0 +1,57 @@
+// let num = 1235;
+// let num1 = 123.45;
+// console.log(num);
+// console.log(num1);
+
+// let num = new Number(123);
+// console.log(num);
+
+// ------------------------- Number properties -------------------------
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.EPSILON);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.NaN);
+// console.log(Number.POSITIVE_INFINITY);
+
+// ------------------------ number methods -------------------------------//
+// let num = 123.45;
+// console.log(parseInt(num));
+// console.log(parseFloat(num));
+// console.log(Number.isFinite(num));
+// console.log(num.toFixed(2));
+// console.log(num.toExponential(3));
+// console.log(num.toPrecision(3));
+
+// let x = "234";
+// console.log(x.toString());
+
+
+// MATH OBJECT 
+// console.log(Math);
+// let num = 123.4;
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.abs(10));
+// console.log(Math.abs(-1));
+// console.log(Math.abs(-0));
+// console.log(Math.sqrt(4));
+// console.log(Math.sqrt(9));
+// console.log(Math.pow(10,2));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,3,4,5,6));
+
+
+// random Numbers 
+// function generateRandomNumbers(min, max){
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(generateRandomNumbers(5,10));
+// console.log(generateRandomNumbers(5,10));
+// console.log(generateRandomNumbers(5,10));
+// console.log(generateRandomNumbers(5,10));
+// console.log(generateRandomNumbers(5,10));
+// console.log(generateRandomNumbers(5,10));
+
+
